fa62848b08faf72fe3b203f83d222963879ad5dc
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-12-12 13:29:42 -0700

    materialize and about route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
   end
+
   def show
-  
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "home#index"
-
+  get 'home', to: 'home#show'
   resources :lists
   resources :items
 
-  resources :home
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
